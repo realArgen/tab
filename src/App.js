31a3f840256436d44a160d4871ec7956016b30ab
Argen <argen.bin.ich@gmail.com>
2021-12-15 03:29:33 +0600

    the logical is finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
 import './App.css';
 
+const url = 'https://course-api.com/react-tabs-project';
+
 function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchJobs()
+
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
+
+  const { company, dates, duties, title } = jobs[value];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Experience</h2>
+      <div className="job-center">
+        <div className="btn-container">
+          {
+            jobs.map((item, index) => {
+              return (
+                <button
+                  key={item.id}
+                  onClick={() => setValue(index)}
+                  className='job-btn'
+                >
+
+                  {item.company}
+                </button>
+              )
+            })
+          }
+        </div>
+        <div className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {
+            duties.map((duty, index) => {
+              return (
+                <div key={index} className='job-desc'>
+                  <FaAngleDoubleRight />
+                  <p>{duty}</p>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
